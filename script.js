eb5650ff39879a1294b1fba6a42f0a15349dbff9
TorenR <torentremaine@outlook.com>
2023-07-17 21:50:51 -0400

    retouched password validation functions

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 let password = document.getElementById("password");
 confirmPassword = document.getElementById("confirmPassword");
 
-function validatePassword() {
+function validatePasswordBtn() {
+	let validator = document.getElementById("validator") ?? null;
 	if (password.value !== confirmPassword.value) {
-		alert("Passwords do not match. Try Again");
 		return false;
 	} else {
+		if (validator) {
+			validator.remove();
+		}
 		return true;
 	}
 }
+
+confirmPassword.addEventListener("input", () => {
+	let validator = document.getElementById("validator") ?? null;
+	if (password.value !== confirmPassword.value) {
+		if (validator) {
+			validator.remove();
+		}
+		confirmPassword.insertAdjacentHTML(
+			"afterend",
+			"<span id=validator><br>*Passwords do not match</span>"
+		);
+	} else {
+		if (validator) {
+			validator.remove();
+		}
+	}
+});
